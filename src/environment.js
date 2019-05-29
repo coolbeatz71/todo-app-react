12714dd9b36a1369354027242aeb64e0704ee051
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-05-29 02:30:21 +0200

    [Feature] Refactor the whole project to support redux

---
+++ src/environment.js
@@ -0,0 +1,3 @@
+export const GET_URL = 'http://jsonplaceholder.typicode.com/todos?_limit=4';
+export const ADD_URL = 'http://jsonplaceholder.typicode.com/todos';
+export const DELETE_URL = 'http://jsonplaceholder.typicode.com/todos/';
---
+++ src/redux/actions/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import * as types from '../action-types';
+import * as env from '../../environment';
+
+export const addTodo = title => dispatch => {
+  // Post a todo to the server using its title
+  axios
+    .post(env.ADD_URL, {
+      title,
+      completed: false,
+    })
+    .then(res => {
+      return dispatch({
+        type: types.ADD_TODO,
+        payload: res.data,
+      });
+    });
+};
+
+export const deleteTodo = id => dispatch => {
+  // Delete a todo from the server using its ID
+  axios.delete(`${env.DELETE_URL}${id}`).then(() => {
+    return dispatch({
+      type: types.DELETE_TODO,
+      payload: id,
+    });
+  });
+};
+
+export const getTodos = () => dispatch => {
+  // Get todos from the server
+  axios.get(env.GET_URL).then(res => {
+    return dispatch({
+      type: types.GET_TODOS,
+      payload: res.data,
+    });
+  });
+};
+
+export const checkTodo = payload => ({
+  type: types.CHECK_TODO,
+  payload,
+});
